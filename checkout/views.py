e8f3d417065085bc2418a02c048cd632e028b98d
PSebastian96 <>
2023-11-07 10:15:37 +0000

    stripe payment

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51O9m4RJck8aA9DpqIeVuLhc34WmnU3em0Xam42wnd9f2UTqOJOSqbVVbfGktKO6iReQ8Npa9PmqCf2HX6KfFNt8a00gTkxWMVf',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
