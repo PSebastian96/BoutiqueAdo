d35f0afdfabf6a33c42724041cfc95e0e0be5250
PSebastian96 <>
2023-11-08 11:23:09 +0000

    fixes

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-psebastian96-boutiqueado-hjcl5kc96m.us2.codeanyapp.com']
 
 
 # Application definition
