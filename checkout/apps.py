1d0e18bd30b8827c2dd0f8706bcc06f9a1d5619a
PSebastian96 <>
2023-11-16 21:27:22 +0000

    checkout success error

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render, redirect, reverse
-from django.contrib import messages
+from django.apps import AppConfig
 
+
+class CheckoutConfig(AppConfig):
+    name = 'checkout'
+
+
+    def ready(self):
+        import checkout.signals
